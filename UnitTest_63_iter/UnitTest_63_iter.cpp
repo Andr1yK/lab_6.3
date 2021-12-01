236064348d2f89bce6e81b5cd3ddf3d3d5076e19
Andriy Kobetiak <andriykobetiak@gmail.com>
2021-12-01 18:26:56 +0200

    Добавьте файлы проекта.

---
+++ UnitTest_63_iter/UnitTest_63_iter.cpp
@@ -0,0 +1,34 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab_6.3/lab_6.3_iter.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest63iter
+{
+	TEST_CLASS(UnitTest63iter)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			const int n = 10;
+			int a[n] = { -1, 0, -4, 5, 6, 2, 10,7, 9, -10 };
+
+			int SortedA[n] = { -10,-4, -1, 0, 2, 5, 6, 7, 9, 10 };
+
+			Sort(a, n);
+
+			for (int i = 0; i < n; i++)
+				Assert::AreEqual(SortedA[i], a[i]);
+
+			double b[n] = { -0.1, 0.3, -2.0, -0.002, 0, 0.9, 1.4, 1., 40.3, -100. };
+			double SortedB[n] = { -100., -2, -0.1, -0.002, 0, 0.3, 0.9, 1., 1.4, 40.3 };
+			
+			Sort(b, n);
+
+			for (int i = 0; i < n; i++)
+				Assert::AreEqual(SortedB[i], b[i]);
+		}
+	};
+}
